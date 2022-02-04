fa85033eaa2f604b0d01ab49f2af9ca28748f881
LancerMar <602545140@qq.com>
2022-02-04 11:42:56 +0000

    chapter 7 except exercise last half

--- populate_rango.py
+++ populate_rango.py
@@ -12,25 +12,33 @@ def populate():
 
     python_pages = [
         {'title': 'Official Python Tutorial',
-        'url':'http://docs.python.org/3/tutorial/'},
+        'url':'http://docs.python.org/3/tutorial/',
+        'views':'300'},
         {'title':'How to Think like a Computer Scientist',
-         'url':'http://www.greenteapress.com/thinkpython/'},
+         'url':'http://www.greenteapress.com/thinkpython/',
+         'views':'200'},
         {'title':'Learn Python in 10 Minutes',
-         'url':'http://www.korokithakis.net/tutorials/python/'} ]
+         'url':'http://www.korokithakis.net/tutorials/python/',
+         'views':'320'} ]
 
     django_pages = [
         {'title':'Official Django Tutorial',
-         'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
+         'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/',
+         'views':'500'},
         {'title':'Django Rocks',
-         'url':'http://www.djangorocks.com/'},
+         'url':'http://www.djangorocks.com/',
+         'views':'180'},
         {'title':'How to Tango with Django',
-         'url':'http://www.tangowithdjango.com/'} ]
+         'url':'http://www.tangowithdjango.com/',
+         'views':'20'} ]
 
     other_pages = [
         {'title':'Bottle',
-         'url':'http://bottlepy.org/docs/dev/'},
+         'url':'http://bottlepy.org/docs/dev/',
+         'views':'110'},
         {'title':'Flask',
-         'url':'http://flask.pocoo.org'} ]
+         'url':'http://flask.pocoo.org',
+         'views':'120'} ]
 
     cats = {'Python': {'pages': python_pages,'views':'128','likes':'64'},
             'Django': {'pages': django_pages,'views':'64','likes':'32'},
@@ -39,7 +47,7 @@ def populate():
     for cat, cat_data in cats.items():
         c = add_cat(cat,cat_data['views'],cat_data['likes'])
         for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'])
+            add_page(c, p['title'], p['url'],p['views'])
 
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
@@ -52,7 +60,7 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name,views,likes):
+def add_cat(name,views=0,likes=0):
     c = Category.objects.get_or_create(name=name)[0]
     c.views = views
     c.likes = likes
